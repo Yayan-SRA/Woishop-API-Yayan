97692d0488ee32b54eaaac59765cb2d418ec1211
Yayan-SRA <yayansra14@gmail.com>
2022-09-30 18:57:38 +0700

    fix

--- app/controllers/api/v1/AuthController.js
+++ app/controllers/api/v1/AuthController.js
@@ -3,44 +3,7 @@ const {User} = require("../../../models");
 const jwt = require("jsonwebtoken");
 const shortid = require('shortid');
 const axios = require("axios").default;
-const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal')
-const client = new Client({
-    authStrategy: new LocalAuth(),
-    restartOnAuthFail: true,
-    puppeteer: { headless: true }
-});
-
-client.initialize();
-
-client.on('loading_screen', (percent, message) => {
-    console.log('LOADING SCREEN', percent, message);
-});
-
-client.on('qr', (qr) => {
-    // NOTE: This event will not be fired if a session is specified.
-    console.log('QR RECEIVED', qr)
-    qrcode.generate(qr, {small: true});
-});
-
-client.on('authenticated', () => {
-    console.log('AUTHENTICATED');
-});
-
-client.on('auth_failure', msg => {
-    // Fired if session restore was unsuccessful
-    console.error('AUTHENTICATION FAILURE', msg);
-});
-
-client.on('ready', () => {
-    console.log('READY');
-});
-
-client.on('disconnected', (reason) => {
-    // Destroy and reinitialize the client when disconnected
-    client.destroy();
-    client.initialize();
-  });
 
 class authController {
     coba = (req,res) => {
@@ -107,40 +70,40 @@ class authController {
             })
     }
 
-    registration = (req,res) => {
-        authService
-        .registration(req.body)
-        .then((data) => {
-            const phone = data.phone_number
-            const otp = Math.floor(1000 + Math.random() * 90000)
-            const msg = `Here is your otp code ${otp}, Don't share with anyone. You register on WoiShop App. If this isn't you, you can ignore it`
-            client.sendMessage(phone+"@c.us", msg).then(()=>{ 
-                const userId = data.id
-                authService
-                .update({userId},{
-                    otp:otp
-                }).then(()=>{
-                    res.status(200).json({
-                        msg: "Resgister Success",
-                        otp: otp,
-                        phone_number: phone
-                    })
-                })
-            })
-            .catch((err)=>{
-                res.status(422).json({
-                    status: "Fail",
-                    messager: err.message
-                })
-            })
-        })
-        .catch((err)=>{
-            res.status(422).json({
-                status: "Fail",
-                messager: err.message
-            })
-        })
-    }
+    // registration = (req,res) => {
+    //     authService
+    //     .registration(req.body)
+    //     .then((data) => {
+    //         const phone = data.phone_number
+    //         const otp = Math.floor(1000 + Math.random() * 90000)
+    //         const msg = `Here is your otp code ${otp}, Don't share with anyone. You register on WoiShop App. If this isn't you, you can ignore it`
+    //         client.sendMessage(phone+"@c.us", msg).then(()=>{ 
+    //             const userId = data.id
+    //             authService
+    //             .update({userId},{
+    //                 otp:otp
+    //             }).then(()=>{
+    //                 res.status(200).json({
+    //                     msg: "Resgister Success",
+    //                     otp: otp,
+    //                     phone_number: phone
+    //                 })
+    //             })
+    //         })
+    //         .catch((err)=>{
+    //             res.status(422).json({
+    //                 status: "Fail",
+    //                 messager: err.message
+    //             })
+    //         })
+    //     })
+    //     .catch((err)=>{
+    //         res.status(422).json({
+    //             status: "Fail",
+    //             messager: err.message
+    //         })
+    //     })
+    // }
 
     activateAccount = (req, res) => {
         const phone = req.params.phone
@@ -171,53 +134,53 @@ class authController {
         })
     }
 
-    login = (req,res) => {
-        const phone_number = req.body.phone_number
-        authService
-        .login({phone_number})
-        .then((account)=>{
-            const userId = account.id;
-            const name = account.full_name;
-            const phone_number = account.phone_number;
-            const otp = Math.floor(Math.random() * 10000)
-            const accessToken = jwt.sign({userId, name, phone_number}, process.env.ACCESS_TOKEN_SECRET, {
-                expiresIn: '2000s'
-            }) 
-            const msg = `Here is your otp code ${otp}, Don't share with anyone. If this isn't you, you can ignore it`
-            const result = client.sendMessage(phone_number+"@c.us", msg)
-                .then(response=>{
-                    if(response){
-                    const coba= authService.update({userId}, {
-                        otp:otp,
-                        })
-                        if(coba) return res.status(200).json({
-                            status: "Succes",
-                            otp: otp,
-                            phone_number: phone_number,
-                            msg:"please input your otp code to continue login",
+    // login = (req,res) => {
+    //     const phone_number = req.body.phone_number
+    //     authService
+    //     .login({phone_number})
+    //     .then((account)=>{
+    //         const userId = account.id;
+    //         const name = account.full_name;
+    //         const phone_number = account.phone_number;
+    //         const otp = Math.floor(Math.random() * 10000)
+    //         const accessToken = jwt.sign({userId, name, phone_number}, process.env.ACCESS_TOKEN_SECRET, {
+    //             expiresIn: '2000s'
+    //         }) 
+    //         const msg = `Here is your otp code ${otp}, Don't share with anyone. If this isn't you, you can ignore it`
+    //         const result = client.sendMessage(phone_number+"@c.us", msg)
+    //             .then(response=>{
+    //                 if(response){
+    //                 const coba= authService.update({userId}, {
+    //                     otp:otp,
+    //                     })
+    //                     if(coba) return res.status(200).json({
+    //                         status: "Succes",
+    //                         otp: otp,
+    //                         phone_number: phone_number,
+    //                         msg:"please input your otp code to continue login",
                             
-                        })
-                    }else if(!response){
-                        res.status(500).json({
-                            status: false,
-                            response:err
-                        })
-                    }
-            }).catch(err => {
-                        res.status(500).json({
-                            status: false,
-                            response:err
-                        })
-                    })
-        })
-        .catch((err)=>{
-            res.status(422).json({
-                status: "Fail",
-                messager: err.message
-            })
-        })
+    //                     })
+    //                 }else if(!response){
+    //                     res.status(500).json({
+    //                         status: false,
+    //                         response:err
+    //                     })
+    //                 }
+    //         }).catch(err => {
+    //                     res.status(500).json({
+    //                         status: false,
+    //                         response:err
+    //                     })
+    //                 })
+    //     })
+    //     .catch((err)=>{
+    //         res.status(422).json({
+    //             status: "Fail",
+    //             messager: err.message
+    //         })
+    //     })
 
-    }
+    // }
 
     inputOtp = (req, res) => {
         try {
