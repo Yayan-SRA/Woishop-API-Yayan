cc8b816ac5fa54cd2fc94141b0acc67fcf46b617
Yayan-SRA <yayansra14@gmail.com>
2022-09-30 15:02:24 +0700

    fix

--- app/index.js
+++ app/index.js
@@ -3,7 +3,6 @@ const cookieParser = require('cookie-parser')
 // const cors = require("cors")
 const morgan = require("morgan");
 const router = require("../config/routes");
-const session = require('client-sessions');
 
 const app = express();
 app.use(cookieParser())
@@ -17,18 +16,6 @@ router.use(express.static(__dirname + "./public/"))
 //middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
-
-
-// app.use(session({
-//     cookieName: 'session',
-//     secret: 'random string',
-//     duration: 30 * 60 * 1000,
-//     activeDuration: 5 * 60 * 1000,
-// }));
-
-/** Install JSON request parser */
-app.use(express.json());
 
 // Bilang ke express kalo kita mau
 // pake EJS sebagai view engine
